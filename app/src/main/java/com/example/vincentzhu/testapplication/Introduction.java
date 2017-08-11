e0f14940938209165a737ea80e53c48fdcc0056a
Thomas <tahmoures@hotmail.com>
2017-08-10 20:24:33 -0700

    This version Fixes the Followin problems and Features : 1. Fix the problem with over flow menu on the introduction page 2. Logout button is now function in over flow menu

--- app/src/main/java/com/example/vincentzhu/testapplication/Introduction.java
+++ app/src/main/java/com/example/vincentzhu/testapplication/Introduction.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -51,11 +52,28 @@ public class Introduction extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    public boolean onCreateOptionMenu (Menu menu){
+    public boolean onCreateOptionsMenu (Menu menu){
        super.onCreateOptionsMenu(menu);
         MenuInflater mMenuInflater = getMenuInflater();
         mMenuInflater.inflate(R.menu.my_menu, menu);
         return true;
+    }
+
+    //Set the function of the overflow key menus such as logout
+    public boolean onOptionsItemSelected(MenuItem item){
+        int selectedId=item.getItemId();
+        switch (selectedId){
+            case R.id.action_about_us:
+
+            case R.id.action_logout:
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+
+
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 
     @Override
