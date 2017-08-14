322cd5868377990d9461b4d0616cf3f829c9a46c
Thomas <tahmoures@hotmail.com>
2017-08-14 16:41:06 -0700

    This version Fixes the Followin problems and Features : 1. Created an admin login page that is accessible from a specfic user account

--- app/src/main/java/com/example/vincentzhu/testapplication/Admin.java
+++ app/src/main/java/com/example/vincentzhu/testapplication/Admin.java
@@ -7,59 +7,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Introduction extends AppCompatActivity implements View.OnClickListener {
+public class Admin extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
-    private TextView textViewUserEmail;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introduction);
-        String mainAccount = "devbizrecipe@gmail.com";
+        setContentView(R.layout.activity_admin);
 
-        // Create toolbar
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser()==null){
-            //Profile activity here
-            finish();
-            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-        }
 
-        FirebaseUser user= firebaseAuth.getCurrentUser();
-
-        if(user.getEmail().equals(mainAccount)){
-            //Profile activity here
-            finish();
-            startActivity(new Intent(getApplicationContext(),Admin.class));
-        }
-
-        textViewUserEmail= (TextView) findViewById(R.id.textViewUserEmail);
-        textViewUserEmail.setText("Welcome "+user.getEmail());
-
-        Button btn = (Button)findViewById(R.id.start); //type cast
-
-        //..set what happens when the user clicks
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Introduction.this, Home.class));
-            }
-        });
     }
 
-    // Create overflow menu
     public boolean onCreateOptionsMenu (Menu menu){
-       super.onCreateOptionsMenu(menu);
+        super.onCreateOptionsMenu(menu);
         MenuInflater mMenuInflater = getMenuInflater();
         mMenuInflater.inflate(R.menu.my_menu, menu);
         return true;
@@ -95,7 +63,4 @@ public class Introduction extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void onClick(View view) {
-
-    }
 }
